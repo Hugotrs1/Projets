2cc9c51234a937c47ab7e65480b57267d167351d
Hugotrs1 <163901405+Hugotrs1@users.noreply.github.com>
2024-04-16 10:56:15 +0200

    Update Combat_poo.py

--- Combat_poo.py
+++ Combat_poo.py
@@ -1,13 +1,12 @@
 import random as rd
-import time
 from tkinter import *
 from PIL import Image, ImageTk
 import pygame
 import os
-
+import time
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-combat_poo_dir = os.path.join(script_dir)
+affichage = os.path.join(script_dir)
 
 
 class Personnage:
@@ -27,7 +26,7 @@ class Personnage:
     def hook(self):
         self.vie -= 15
 
-class CombatApplication:
+class Combat:
     def __init__(self, fenetre):
         self.fenetre = fenetre
         self.can = Canvas(self.fenetre, width=1000, height=680, bg='white')
@@ -39,9 +38,10 @@ class CombatApplication:
         self.photo = ImageTk.PhotoImage(image)
         self.can.delete("all")
         self.can.create_image(0, 0, anchor=NW, image=self.photo)
+        fen.update()
 
 
-def play_sound(sound_file):
+def son(sound_file):
     pygame.mixer.init()
     pygame.mixer.music.load(sound_file)
     pygame.mixer.music.play()
@@ -67,6 +67,7 @@ def combat():
         hookproba = rd.randint(1, 7)
         uppercutproba = rd.randint(1, 10)
 
+
         if atckfrst == 1:
             choixatt = input('Quel coup voulez vous envoyer ? jab, uppercut, hook ')
             if choixatt == 'jab':
@@ -74,40 +75,44 @@ def combat():
                     perso2.jab()
                     jp1+=1
                     print(perso1.nom, 'envoie un jab')
-                    play_sound(os.path.join(combat_poo_dir, "Jab_sound.mp3"))
-                    app.afficher(os.path.join(combat_poo_dir, "P1_jab_p2.png"))
+                    son(os.path.join(affichage, "Jab_sound.mp3"))
+                    aff.afficher(os.path.join(affichage, "P1_jab_p2.png"))
+
 
                 else:
                     jesp1+=1
                     print(perso1.nom, 'a raté son coup')
-                    play_sound(os.path.join(combat_poo_dir, "esquive.mp3"))
-                    app.afficher(os.path.join(combat_poo_dir, "P2_esj_p1.png"))
+                    son(os.path.join(affichage, "esquive.mp3"))
+                    aff.afficher(os.path.join(affichage, "P2_esj_p1.png"))
+
 
             if choixatt == 'uppercut':
                 if uppercutproba > 8:
                     perso2.uppercut()
                     up1+=1
                     print(perso1.nom, 'décroche un uppercut')
-                    play_sound(os.path.join(combat_poo_dir, "final_punch.mp3"))
-                    app.afficher(os.path.join(combat_poo_dir, "P1_up_p2.png"))
+                    son(os.path.join(affichage, "final_punch.mp3"))
+                    aff.afficher(os.path.join(affichage, "P1_up_p2.png"))
+
                 else:
                     upes1+=1
                     print(perso1.nom, 'a raté son coup')
-                    play_sound(os.path.join(combat_poo_dir, "esquive.mp3"))
-                    app.afficher(os.path.join(combat_poo_dir, "P2_esu_p1.png"))
+                    son(os.path.join(affichage, "esquive.mp3"))
+                    aff.afficher(os.path.join(affichage, "P2_esu_p1.png"))
 
             if choixatt == 'hook':
                 if hookproba > 3:
                     perso2.hook()
                     h1+=1
                     print(perso1.nom, 'qui corrige', perso2.nom, 'avec un crochet')
-                    play_sound(os.path.join(combat_poo_dir, "hook_sound.mp3"))
-                    app.afficher(os.path.join(combat_poo_dir, "P1_hook_p2.png"))
+                    son(os.path.join(affichage, "hook_sound.mp3"))
+                    aff.afficher(os.path.join(affichage, "P1_hook_p2.png"))
+
                 else:
                     hes1+=1
                     print(perso1.nom, 'a raté son coup')
-                    play_sound(os.path.join(combat_poo_dir, "esquive.mp3"))
-                    app.afficher(os.path.join(combat_poo_dir, "P2_esh_p1.png"))
+                    son(os.path.join(affichage, "esquive.mp3"))
+                    aff.afficher(os.path.join(affichage, "P2_esh_p1.png"))
 
         elif atckfrst == 2:
             c = rd.randint(1, 3)
@@ -116,67 +121,80 @@ def combat():
                     perso1.jab()
                     jp2+=1
                     print(perso2.nom, 'envoie un jab')
-                    play_sound(os.path.join(combat_poo_dir, "Jab_sound.mp3"))
-                    app.afficher(os.path.join(combat_poo_dir, "P2_jab_p1.png"))
+                    son(os.path.join(affichage, "Jab_sound.mp3"))
+                    aff.afficher(os.path.join(affichage, "P2_jab_p1.png"))
+
                 else:
                     jesp2+=1
                     print(perso2.nom, 'a raté son coup')
-                    play_sound(os.path.join(combat_poo_dir, "esquive.mp3"))
-                    app.afficher(os.path.join(combat_poo_dir, "P1_esj_p2.png"))
+                    son(os.path.join(affichage, "esquive.mp3"))
+                    aff.afficher(os.path.join(affichage, "P1_esj_p2.png"))
+
 
             elif c == 2:
                 if uppercutproba > 8:
                     perso1.uppercut()
                     up2+=1
                     print(perso2.nom, 'décroche un uppercut')
-                    play_sound(os.path.join(combat_poo_dir, "final_punch.mp3"))
-                    app.afficher(os.path.join(combat_poo_dir, "P2_up_p1.png"))
+                    son(os.path.join(affichage, "final_punch.mp3"))
+                    aff.afficher(os.path.join(affichage, "P2_up_p1.png"))
+                    fen.after(2000,fen.update)
                 else:
                     upes2+=1
                     print(perso2.nom, 'a raté son coup')
-                    play_sound(os.path.join(combat_poo_dir, "esquive.mp3"))
-                    app.afficher(os.path.join(combat_poo_dir, "P1_esu_p2.png"))
+                    son(os.path.join(affichage, "esquive.mp3"))
+                    aff.afficher(os.path.join(affichage, "P1_esu_p2.png"))
+
 
             elif c == 3:
                 if hookproba > 3:
                     perso1.hook()
                     h2+=1
                     print(perso2.nom, 'qui corrige', perso1.nom, 'avec un crochet')
-                    play_sound(os.path.join(combat_poo_dir, "hook_sound.mp3"))
-                    app.afficher(os.path.join(combat_poo_dir, "P2_hook_p1.png"))
+                    son(os.path.join(affichage, "hook_sound.mp3"))
+                    aff.afficher(os.path.join(affichage, "P2_hook_p1.png"))
+
                 else:
                     hes2+=1
                     print(perso2.nom, 'a raté son coup')
-                    play_sound(os.path.join(combat_poo_dir, "esquive.mp3"))
-                    app.afficher(os.path.join(combat_poo_dir, "P1_esh_p2.png"))
+                    son(os.path.join(affichage, "esquive.mp3"))
+                    aff.afficher(os.path.join(affichage, "P1_esh_p2.png"))
 
 
-        fen.after(2000,fen.update_idletasks)
+
+        time.sleep(1)
         print('-------------------------------')
         print('PV de', perso1.nom, ':', perso1.pvrestant())
         print('PV de', perso2.nom, ':', perso2.pvrestant())
         print('-------------------------------')
+        time.sleep(2)
+
 
 
     if perso2.pvrestant() <= 0:
         print(perso1.nom, 'remporte le combat')
         recap(jp1, jesp1, up1, upes1, h1, hes1, jp2, jesp2, up2, upes2, h2, hes2)
         fen.title(perso1.nom +' à gagné')
-        play_sound(os.path.join(combat_poo_dir, "final_punch.mp3"))
-        app.afficher(os.path.join(combat_poo_dir, "p1 KO p2.png"))
-        fen.after(4000, fen.update_idletasks)
-        play_sound(os.path.join(combat_poo_dir, "Bell.mp3"))
-        app.afficher(os.path.join(combat_poo_dir, "p1_win.png"))
+        son(os.path.join(affichage, "final_punch.mp3"))
+        aff.afficher(os.path.join(affichage, "p1 KO p2.png"))
+        time.sleep(2)
+        son(os.path.join(affichage, "Bell.mp3"))
+        aff.afficher(os.path.join(affichage, "p1_win.png"))
+        time.sleep(10)
+        fen.destroy()
+
 
     if perso1.pvrestant() <= 0:
         print(perso2.nom, 'remporte le combat')
         recap(jp1, jesp1, up1, upes1, h1, hes1, jp2, jesp2, up2, upes2, h2, hes2)
         fen.title(perso2.nom +' à gagné')
-        play_sound(os.path.join(combat_poo_dir, "final_punch.mp3"))
-        app.afficher(os.path.join(combat_poo_dir, "p2 KO p1.png"))
-        fen.after(4000, fen.update_idletasks)
-        play_sound(os.path.join(combat_poo_dir, "Bell.mp3"))
-        app.afficher(os.path.join(combat_poo_dir, "p2_win.png"))
+        son(os.path.join(affichage, "final_punch.mp3"))
+        aff.afficher(os.path.join(affichage, "p2 KO p1.png"))
+        time.sleep(2)
+        son(os.path.join(affichage, "Bell.mp3"))
+        aff.afficher(os.path.join(affichage, "p2_win.png"))
+        time.sleep(10)
+        fen.destroy()
     fen.mainloop()
 
 
@@ -188,9 +206,8 @@ perso1 = Personnage(nom_perso1, 75)
 perso2 = Personnage(nom_perso2, 75)
 fen = Tk()
 fen.title('Combat Start')
-app = CombatApplication(fen)
-app.afficher(os.path.join(combat_poo_dir,"Start.png"))
-fen.after(6000,combat)
-fen.update_idletasks()
+aff = Combat(fen)
+aff.afficher(os.path.join(affichage,"Start.png"))
+fen.after(4000,combat)
 
 fen.mainloop()
